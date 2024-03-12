b741b99fc21ef265da0c4b4a34c80e61ab3c883a
sinaazadfar <sinaazadfar@gmail.com>
2024-03-12 12:40:27 +0330

    sina

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -2,7 +2,7 @@
   <v-app height="100%">
     <v-card height="100%" elevation="0">
 
-      <v-window v-model="step" class=" h-100">
+      <v-window v-model="step"   state='login' class=" h-100">
         <v-window-item :value="1" class=" h-100">
           <v-row class="h-100">
             <v-col cols="12" md="6" class="h-100">
@@ -80,7 +80,7 @@
                   </div>
                 </v-card-text>
                 <div class="text-center">
-                  <v-btn tile rounded variant="outlined" dark @click="step++" class="mt-10 font-weight-bold px-15">ثبت
+                  <v-btn  tile rounded variant="outlined"  @click="step++ ; state='signup'" class="mt-10 font-weight-bold px-15">ثبت
                     نام</v-btn>
                 </div>
               </div>
@@ -94,11 +94,11 @@
                 <v-card-text class="white--text">
                   <h3 class="text-center font-weight-black text-h3 rtl">اکانتی داری؟</h3>
                   <div class="text-center mt-3">
-                    از قبل اکانتی برای پنل ادمین به ثبت رسانده اید؟
+                    از قبل اکانتی به ثبت رسانده اید؟
                   </div>
                 </v-card-text>
                 <div class="text-center">
-                  <v-btn tile rounded variant="outlined" dark @click="step--" class="mt-10 font-weight-bold px-15">ورود
+                  <v-btn tile rounded variant="outlined" dark @click="step-- ; state='login'" class="mt-10 font-weight-bold px-15">ورود
                   </v-btn>
 
                 </div>
@@ -108,30 +108,68 @@
               <v-card-text class="mt-12">
                 <h4 class="text-center text-h4 my-10 font-weight-black">ثبت نام </h4>
                 <v-locale-provider rtl>
-                  <v-row align="center" justify="center">
-                    <v-col cols="12" sm="8">
-                      <v-row>
-                        <v-col cols="12" sm="6">
-                          <v-text-field label="نام" rounded="lg" persistent-hint variant="outlined" color="primary"
-                            class="mt-4" />
-                        </v-col>
-                        <v-col cols="12" sm="6">
-                          <v-text-field label="نام خانوادگی" rounded="lg" persistent-hint variant="outlined"
-                            color="primary" class="mt-4" />
-                        </v-col>
-                      </v-row>
-                      <v-text-field label="ایمیل" rounded="lg" persistent-hint variant="outlined" color="primary" />
-                      <v-text-field label="شماره تلفن" rounded="lg" persistent-hint variant="outlined" color="primary" />
-                      <v-text-field label="پسورد" rounded="lg" persistent-hint variant="outlined" color="primary"
-                        type="password" />
+                  <form v-if="state == 'signup' " @submit.prevent="sendSingUpSms">
+                    <v-row align="center" justify="center">
+                      <v-col cols="12" sm="8">
+                        <v-row>
+                          <v-col cols="12" sm="6">
+                            <v-text-field v-model="first_name" label="نام" rounded="lg" persistent-hint variant="outlined" color="primary"
+                              class="mt-4" />
+                          </v-col>
+                          <v-col cols="12" sm="6">
+                            <v-text-field v-model="last_name"  label="نام خانوادگی" rounded="lg" persistent-hint variant="outlined"
+                              color="primary" class="mt-4" />
+                          </v-col>
+                        </v-row>
+                        <v-text-field v-model="phoneNumber" label="شماره تلفن" rounded="lg" persistent-hint variant="outlined" color="primary" />
+                    
+                        <v-btn :loading="loading" class="mt-16" size="large" elevation="0" rounded color="primary" dark block tile>ثبت
+                          نام</v-btn>                    
+                      </v-col>
+                    </v-row>
+                  </form>
+                  <form v-if="state == 'get_code_signup' " @submit.prevent="check_code_signup" >
+                    <v-row align="center" justify="center">
+                      <v-col cols="12" sm="8">
+                        <div v-if="error" class="  text-red-accent-4 d-flex justify-start pl-3 pt-1 ">
+                        {{ error }}
+                        </div>
+                        <v-text-field
+                          v-model="code"
+                          :rules="[rules.required, rules.number]"
+                        label="کد را وارد کنید" required rounded="lg"
+                        persistent-hint variant="outlined"
+                          color="primary" class="mt-16" />
+                      <div @click="state='login'"  class="  text-indigo-accent-4 d-flex justify-end pl-3 pt-1 cursor-pointer">
+                        تغییر شماره  {{ phoneNumber }} 
+                      </div>
+                        
 
-                      <v-btn class="mt-16" size="large" elevation="0" rounded color="primary" dark block tile>ثبت
-                        نام</v-btn>
-
-
-
-                    </v-col>
-                  </v-row>
+                        <v-btn
+                          type="submit"
+                          class="mt-16"
+                          size="large"
+                          elevation="0"
+                          rounded
+                          color="primary"
+                          :loading="loading"
+                          dark
+                          block
+                          tile>
+                          
+                        <div >
+                          ورود
+                        </div>
+                        </v-btn>
+                        <div v-if="isCooldownActive" class="text-indigo-accent-2 d-flex  justify-center pt-5">
+                          {{ Math.floor(cooldownTime / 60) }}:{{ ('0' + cooldownTime % 60).slice(-2) }}  تا تلاش مجدد
+                        </div>
+                        <div v-else @click="state='login'" class="text-indigo-accent-4 d-flex justify-center cursor-pointer pt-5">
+                          دریافت مجدد کد
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </form>
                 </v-locale-provider>
               </v-card-text>
             </v-col>
@@ -156,7 +194,9 @@ export default {
     isCooldownActive: false,
     cooldownTime: 120, // 2 minutes in seconds
     state: 'login',
-    step: 1,
+    first_name: '',
+    last_name: '',
+    step: null,
     rules: {
       required: value => !!value || 'این فیلد اجباری است',
       phoneNumber: value => {
@@ -272,7 +312,79 @@ export default {
             console.error('Phone number is empty');
             // You can show an error message to the user here
         }
-    }
+    },
+    sendSingUpSms() {
+        // Ensure the phone number is not empty
+        if (this.phoneNumber) {
+            const apiUrl = `${apiStore().address}/api/account/sign-up-sms/`;
+            const data = {
+                first_name: this.first_name, // Assuming the API expects the full number with country code
+                last_name: this.last_name, // Assuming the API expects the full number with country code
+                number: this.phoneNumber // Assuming the API expects the full number with country code
+            };
+            this.loading = true
+            axios.post(apiUrl, data, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+
+                },
+            })
+                .then(response => {
+                    // Handle success response
+                    console.log('SMS sent successfully:', response);
+                    this.state =  'get_code_signup'
+                    this.loading = false
+                    this.isCooldownActive = true;
+                    this.startCooldown();
+                    // You can change the dialog page or show a success message here
+                })
+                .catch(error => {
+                    // Handle error response
+                    console.error('Error sending SMS:', error);
+                    // You can show an error message to the user here
+                });
+        } else {
+            // Phone number is empty, handle accordingly
+            console.error('Phone number is empty');
+            // You can show an error message to the user here
+        }
+    },
+    check_code_signup() {
+        // Ensure the phone number is not empty
+        this.loading = true
+        if (this.code) {
+            const apiUrl = `${apiStore().address}/api/account/code-check-sign-up/`;
+            const data = {
+                number: this.phoneNumber, // Assuming the API expects the full number with country code
+                code: this.code
+            };
+            axios.post(apiUrl, data, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+
+                },
+            })
+                .then(response => {
+                    // Handle success response
+                    const userStore = useUserStore();
+                    userStore.setToken(response.data.token, response.data.username);
+                    this.$emit("close")
+                    
+                    // You can change the dialog page or show a success message here
+                })
+                .catch(error => {
+                    // Handle error response
+                    this.error = 'کد معتبر نیست'
+                    this.loading = false
+                    console.error('Error sending SMS:', error);
+                    // You can show an error message to the user here
+                });
+        } else {
+            // Phone number is empty, handle accordingly
+            console.error('Phone number is empty');
+            // You can show an error message to the user here
+        }
+    },
   }
 }
 </script>
