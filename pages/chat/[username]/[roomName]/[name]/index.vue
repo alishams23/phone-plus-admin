a2fb69fc0433a8e6ad7d8b697326e5800e1351b4
ali <alishams.s1380@gmail.com>
2024-12-10 14:01:23 +0330

    fix chat seen

--- pages/chat/[username]/[roomName]/[name]/index.vue
+++ pages/chat/[username]/[roomName]/[name]/index.vue
@@ -39,10 +39,27 @@
             <div class="d-flex justify-end align-center">
               <div v-if="data.content" class="bg-primary mx-3  rounded-be-0 rounded-pill px-5 py-3">
                 {{ data.content }}
+
+                
               </div>
             </div>
             <v-card class="pe-3" elevation="0" v-if="data.created_at">
               <div class="d-flex justify-end align-center">
+                <div v-if="data.read == 'True'" >
+                  <svg v-if="data.username == userSelf" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    fill="currentColor" class="bi bi-check2-all me-1 mt-1 text-blue-300" viewBox="0 0 16 16">
+                    <path
+                      d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z" />
+                    <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z" />
+                  </svg>
+                </div>
+                <div v-else>
+                  <svg v-if="data.username == userSelf" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    fill="currentColor" class="bi bi-check2 me-1 mt-1 text-gray-400" viewBox="0 0 16 16">
+                    <path
+                      d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                  </svg>
+                </div>
                 <div class="text-grey text-caption">
                   {{ convertToJalaali(data.created_at) }}
                 </div>
@@ -229,6 +246,13 @@ export default {
 
   },
 
+  watch: {
+  $route(to, from) {
+    if (this.chatSocket) {
+      this.chatSocket.close();
+    }
+  }
+},
   computed:{
     address(){
         return apiStore().address
