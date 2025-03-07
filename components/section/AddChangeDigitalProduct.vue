92000eda2de833c3729e897f50a93b31b6ce1797
ali <alishams.s1380@gmail.com>
2025-03-07 23:52:55 +0330

    fix error

--- components/section/AddChangeDigitalProduct.vue
+++ components/section/AddChangeDigitalProduct.vue
@@ -300,7 +300,7 @@
                                             </td>
 
                                             <SharedConfirmationDialog v-if="items.sold == false"
-                                                @delete-item="() => {removeSubsetProduct(items)}">
+                                                @delete-item="() => {subset_product.splice(subset_product.indexOf(items), 1); snackbar_delete = true; removeSubsetProduct(items)}">
                                                 <v-btn class="mb-2  me-2 ms-1" icon color="red" variant="tonal"
                                                     size="small">
                                                     <TrashIcon size="18" />
@@ -564,8 +564,7 @@ export default {
             return transformedData.filter(row => row.length > 0);
         },
         removeSubsetProduct(item) {
-            this.subset_product.splice(subset_product.indexOf(item), 1);
-             this.snackbar_delete = true; 
+           
             fetch(`${apiStore().address}/api/product/seller-panel/remove-row-subset-digital-product/${item.id}`, {
                 method: 'DELETE',
                 headers: {
